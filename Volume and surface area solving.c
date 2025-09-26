cafc89752384943df6044acb0ceb08af124c05ee
vincent-hub-eng <vmutua471@gmail.com>
2025-09-26 10:42:43 +0300

    Add files via upload

---
+++ Volume and surface area solving.c
@@ -0,0 +1,30 @@
+/*
+Name:vincent mutua
+reg:CT100/G/26230/25
+Description:solving for vilume and surface area 
+*/
+
+#include<stdio.h>
+#include<math.h>
+
+
+int main(){
+ float PI=3.142;   
+double radius,height,volume,surface_area;
+
+//prompt user for input 
+printf("Enter the radius of the cylinder:");
+scanf("%lf",&radius);
+
+printf("Enter the height of the cylinder:");
+scanf("%lf",&height);
+
+//calculation 
+volume= PI*pow(radius, 2)*height;
+surface_area=2*PI*radius*height+2*PI*pow(radius, 2);
+
+//output results 
+printf("volume of the cylinder = %.2lf\n",volume);
+printf("surface_area of the cylinder = %.2lf\n",surface_area);
+    return 0;
+}
