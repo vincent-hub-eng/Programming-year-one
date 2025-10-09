cdc9aee9028bf5839f22de4965f1d35b2bff7c0f
vincent-hub-eng <vmutua471@gmail.com>
2025-10-09 18:53:14 +0300

    Add files via upload

---
+++ printing numbers.c
@@ -0,0 +1,18 @@
+/*
+name:Vincent mutua 
+Reg:CT100/G/26230/25
+des:printinumber from 50to 100
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i;
+    
+    // Print numbers from 100 to 50 using a for loop
+    for(i = 100; i >= 50; i--) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
